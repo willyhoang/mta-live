982e9bb1751b1e002756047a5aecec1f86dbda91
Willy Hoang <willy.hoang@blueapron.com>
2017-08-21 17:35:00 -0400

    Initial Commit

---
+++ mta-live/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mta-live
+//
+//  Created by Willy on 8/21/17.
+//  Copyright © 2017 Willy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
